17a48e90e277cb3040c6ed8364e468dfcf828a35
Andreas Nilsson <nilsiker94@gmail.com>
2025-11-23 14:33:00 +0100

    simplify PickupsData by to/from_dict delegation

--- project/save/data/pickups/PickupsData.gd
+++ project/save/data/pickups/PickupsData.gd
@@ -6,21 +6,18 @@ var items: Array[ItemPickupData] = []
 
 
 func to_dict() -> Dictionary:
-	var dict := {}
-	dict.items = []
-	for item_data in items:
-		var item_dict := {}
-		item_dict.position = item_data.position
-		item_dict.item = item_data.item.to_dict()
-		dict.items.append(item_dict)
-	return dict
+	var item_dicts = items.map(
+		func(item_data: ItemPickupData) -> Dictionary: return item_data.to_dict()
+	)
+
+	return {"items": item_dicts}
 
 
 static func from_dict(dict: Dictionary) -> PickupsData:
 	var data := PickupsData.new()
+
 	for item_dict in dict.get("items", []):
-		var item_data := ItemPickupData.new()
-		item_data.position = item_dict.get("position", Vector2.ZERO)
-		item_data.item = ItemData.from_dict(item_dict.get("item", {}))
+		var item_data = ItemPickupData.from_dict(item_dict)
 		data.items.append(item_data)
+
 	return data
