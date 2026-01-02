be28a68a4a85d106b74dbd5d56962f81a4037528
Andreas Nilsson <nilsiker94@gmail.com>
2026-01-02 13:52:48 +0100

    settle for 4-directional, art assets have to wait

--- project/player/DirectionRotatorComponent.gd
+++ project/player/DirectionRotatorComponent.gd
@@ -1,5 +1,21 @@
 class_name DirectionRotatorComponent extends Node2D
 
+enum FaceDirection { UP, DOWN, LEFT, RIGHT }
+
 
 func update_direction(direction: Vector2) -> void:
-	rotation = lerp_angle(rotation, direction.angle(), get_physics_process_delta_time() * 25.0)
+	var angle = direction.angle()
+
+	# prefer left and right over up DOWN
+	if abs(direction.x) >= abs(direction.y):
+		if direction.x > 0:
+			angle = 0  # RIGHT
+		else:
+			angle = PI  # LEFT
+	else:
+		if direction.y > 0:
+			angle = PI / 2  # DOWN
+		else:
+			angle = -PI / 2  # UP
+
+	rotation = angle
